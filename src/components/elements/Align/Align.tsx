97c9a3c1637b4539642cf8cb9dc26b6203fdf13a
thoaint <thoaint@bap.jp>
2022-11-27 13:59:19 +0700

    add Align component

---
+++ src/components/elements/Align/Align.tsx
@@ -0,0 +1,68 @@
+import React, { useCallback, useMemo, useRef, useState } from 'react';
+import { LayoutChangeEvent, ViewStyle } from 'react-native';
+import { ViewProps } from '@/types';
+import { Box } from '../Box';
+
+type Props = {
+  alignment?:
+    | 'bottomCenter'
+    | 'bottomLeft'
+    | 'bottomRight'
+    | 'center'
+    | 'centerLeft'
+    | 'centerRight'
+    | 'topCenter'
+    | 'topLeft'
+    | 'topRight';
+};
+
+const Alignment: any = {
+  bottomCenter: {
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  bottomLeft: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+  },
+  bottomRight: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+  },
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  centerLeft: {
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  centerRight: {
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  topCenter: {
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  topLeft: {
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  topRight: {
+    justifyContent: 'flex-start',
+    alignItems: 'flex-end',
+  },
+};
+
+export const Align: React.FC<Props & ViewProps> = function ({
+  children,
+  alignment = 'bottomLeft',
+  ...props
+}) {
+  return (
+    <Box {...Alignment[alignment]} {...props}>
+      {children}
+    </Box>
+  );
+};
