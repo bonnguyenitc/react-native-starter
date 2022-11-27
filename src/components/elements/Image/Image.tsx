85b9aca427c94fbad158fefc84147493c7e2d54f
thoaint <thoaint@bap.jp>
2022-11-27 13:10:55 +0700

    add Positioned, Stack, Image components

---
+++ src/components/elements/Image/Image.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  ImageResizeMode,
+  ImageSourcePropType,
+  ImageStyle,
+  StyleProp,
+  ViewStyle,
+} from 'react-native';
+import Reanimated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+import { Box } from '../Box';
+
+type Props = {
+  style?: StyleProp<ViewStyle>;
+  source: ImageSourcePropType;
+  resizeMode?: ImageResizeMode;
+};
+
+const IMAGE: ImageStyle = {
+  width: undefined,
+  height: undefined,
+  flex: 1,
+};
+
+export const Image: React.FC<Props> = function (props) {
+  const { style, source, resizeMode = 'contain', ...rest } = props;
+  const x = useSharedValue(0);
+
+  const opacity = useAnimatedStyle(() => ({
+    opacity: x.value,
+  }));
+
+  const handleOnLayout = React.useCallback(() => {
+    x.value = withTiming(1, {
+      duration: 1200,
+      easing: Easing.in(Easing.ease),
+    });
+  }, [x]);
+
+  return (
+    <Box style={style} {...rest}>
+      <Reanimated.Image
+        style={[IMAGE, opacity]}
+        source={source}
+        resizeMode={resizeMode}
+        onLayout={handleOnLayout}
+      />
+    </Box>
+  );
+};
--- src/types/view.ts
+++ src/types/view.ts
@@ -2,19 +2,20 @@ import {
   BackgroundColorProps,
   BorderProps,
   LayoutProps,
+  PositionProps,
   ShadowProps,
   SpacingProps,
 } from '@shopify/restyle';
-import React from 'react';
-import { StyleProp, ViewStyle } from 'react-native';
+import { StyleProp, ViewStyle, ViewProps as ViewPropsRN } from 'react-native';
 import { Theme } from '@/themes';
 
 export interface ChildrenProps {
   children?: React.ReactNode;
 }
-export type ViewProps = { style?: StyleProp<ViewStyle> } & ChildrenProps &
-  SpacingProps<Theme> &
+export type ViewProps = { style?: StyleProp<ViewStyle> } & SpacingProps<Theme> &
   BorderProps<Theme> &
   BackgroundColorProps<Theme> &
   LayoutProps<Theme> &
-  ShadowProps<Theme>;
+  PositionProps<Theme> &
+  ShadowProps<Theme> &
+  ViewPropsRN;
