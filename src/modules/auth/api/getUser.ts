008d8dc7d984c83cbd03ba858120e896808aec5b
thoaint <thoaint@bap.jp>
2022-12-15 10:44:04 +0700

    update

--- src/modules/auth/api/getUser.ts
+++ src/modules/auth/api/getUser.ts
@@ -1,7 +1,16 @@
-import { axios } from '@/shared/libs/axios'
+import { axios, createErrorData } from '@/shared/libs/axios'
+import { ResponseApi } from '@/shared/types/api'
 
 import { AuthUser } from '../types'
 
-export const getUser = (): Promise<AuthUser> => {
-  return axios.get('/auth/me')
+export const getUserService = async (): Promise<ResponseApi<AuthUser, any>> => {
+  try {
+    await axios.get('/auth/me')
+    return {
+      ok: true,
+      data: { email: 'abc@mail.com', bio: '', firstName: '', id: '', lastName: '', role: 'ADMIN' },
+    }
+  } catch (error: any) {
+    return createErrorData(error)
+  }
 }
--- src/shared/libs/axios.ts
+++ src/shared/libs/axios.ts
@@ -1,7 +1,8 @@
-import Axios, { AxiosRequestConfig } from 'axios'
+import Axios, { AxiosError, AxiosRequestConfig } from 'axios'
 import { API_URL } from '@/shared/config'
 import { useNotificationStore } from '@/shared/stores/useNotificationStore'
 import storage from '@/shared/utils/storage'
+import { ResponseApi } from '../types/api'
 
 function authRequestInterceptor(config: AxiosRequestConfig) {
   const token = storage.getAccessToken()
@@ -11,13 +12,14 @@ function authRequestInterceptor(config: AxiosRequestConfig) {
       newConfig.headers.authorization = `${token}`
     }
     newConfig.headers.Accept = 'application/json'
+    newConfig.headers['Content-Type'] = 'application/json'
   }
-
   return newConfig
 }
 
 export const axios = Axios.create({
   baseURL: API_URL,
+  timeout: 5000,
 })
 
 axios.interceptors.request.use(authRequestInterceptor)
@@ -36,3 +38,52 @@ axios.interceptors.response.use(
     return Promise.reject(error)
   },
 )
+
+export const NONE = null
+export const CLIENT_ERROR = 'CLIENT_ERROR'
+export const SERVER_ERROR = 'SERVER_ERROR'
+export const TIMEOUT_ERROR = 'TIMEOUT_ERROR'
+export const CONNECTION_ERROR = 'CONNECTION_ERROR'
+export const NETWORK_ERROR = 'NETWORK_ERROR'
+export const UNKNOWN_ERROR = 'UNKNOWN_ERROR'
+export const CANCEL_ERROR = 'CANCEL_ERROR'
+
+const TIMEOUT_ERROR_CODES = ['ECONNABORTED']
+const NODEJS_CONNECTION_ERROR_CODES = ['ENOTFOUND', 'ECONNREFUSED', 'ECONNRESET']
+
+const inRange = (min: number, max: number, value: number): boolean => value >= min && value <= max
+
+const in200s = (n: number): boolean => inRange(200, 299, n)
+const in400s = (n: number): boolean => inRange(400, 499, n)
+const in500s = (n: number): boolean => inRange(500, 599, n)
+
+type StatusCodes = undefined | number | null
+
+export const getProblemFromStatus = (status: StatusCodes) => {
+  if (!status) return UNKNOWN_ERROR
+  if (in200s(status)) return NONE
+  if (in400s(status)) return CLIENT_ERROR
+  if (in500s(status)) return SERVER_ERROR
+  return UNKNOWN_ERROR
+}
+
+export const getProblemFromError = (error: AxiosError | any) => {
+  if (error.message === 'Network Error') return NETWORK_ERROR
+  if (Axios.isCancel(error)) return CANCEL_ERROR
+  if (!error.code) return getProblemFromStatus(error.response ? error.response.status : null)
+  if (TIMEOUT_ERROR_CODES.includes(error.code)) return TIMEOUT_ERROR
+  if (NODEJS_CONNECTION_ERROR_CODES.includes(error.code)) return CONNECTION_ERROR
+  return UNKNOWN_ERROR
+}
+
+export const createErrorData = (error: any): ResponseApi<any, any> => {
+  return {
+    ok: false,
+    error: {
+      problem: getProblemFromError(error),
+      ...(typeof error?.response?.data === 'string'
+        ? { message: error?.response?.data }
+        : error?.response?.data),
+    },
+  }
+}
