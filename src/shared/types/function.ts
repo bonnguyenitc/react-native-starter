3999e4d8df82e649b8ac619a26b64326bbfb5a6e
Nguyễn Tấn Thoại <thoaint@bap.jp>
2023-01-28 09:57:14 +0700

    add more func type

--- src/shared/types/function.ts
+++ src/shared/types/function.ts
@@ -1,8 +1,10 @@
 export type CallbackFunction<T, V> = (param?: T) => V
 export type CallbackFunctionRequired<T, V> = (param: T) => V
-export type CallbackTwoFunction<T1, T2, V> = (param1?: T1, param2?: T2) => V
-export type CallbackTwoFunctionRequired<T1, T2, V> = (param1: T1, param2: T2) => V
+export type CallbackFunctionParams<T1, T2, V> = (param1?: T1, param2?: T2) => V
+export type CallbackFunctionParamsRequired<T1, T2, V> = (param1: T1, param2: T2) => V
 export type AsyncCallbackFunction<T, V> = (param?: T) => Promise<V>
 export type AsyncCallbackFunctionRequired<T, V> = (param: T) => Promise<V>
-export type AsyncCallbackTwoFunction<T1, T2, V> = (arg1?: T1, arg2?: T2) => Promise<V>
-export type AsyncCallbackTwoFunctionRequired<T1, T2, V> = (arg1: T1, arg2: T2) => Promise<V>
+export type AsyncCallbackFunctionParams<T1, T2, V> = (arg1?: T1, arg2?: T2) => Promise<V>
+export type AsyncCallbackFunctionParamsRequired<T1, T2, V> = (arg1: T1, arg2: T2) => Promise<V>
+export type CallbackFunctionVariadicAnyReturn = (...args: any[]) => any
+export type CallbackFunctionVariadicAnyReturnAsync = (...args: any[]) => Promise<any>
